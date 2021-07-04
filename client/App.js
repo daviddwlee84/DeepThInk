ce4f441b995cc6f8abf0efdb88b30e457bc2c504
Justin <justin.leung.ch@gmail.com>
2021-07-04 08:25:29 -0400

    Get image data from canvas

--- client/App.js
+++ client/App.js
@@ -15,37 +15,49 @@ import axios from 'axios';
 var device = Dimensions.get('window');
 
 // Sends a request to backend when clicking the "generate" button
-function sendRequest() {
-  console.log('Sending API request');
-  Snackbar.show({
-    text: 'Sending API request to server...',
-    duration: Snackbar.LENGTH_SHORT,
-  });
-
-  // Send the request to backend
-  axios.get('http://10.0.2.2:8080/ping').then(function (response) {
-    console.log(response.data.message);
-
-    // Show toast message on bottom of app
-    Snackbar.show({
-      text: 'Received response!',
-      duration: Snackbar.LENGTH_SHORT,
-    });
-  });
-}
 
 export default class App extends Component {
+  sendRequest = () => {
+    console.log('Sending API request');
+    Snackbar.show({
+      text: 'Sending API request to server...',
+      duration: Snackbar.LENGTH_SHORT,
+    });
+
+    this.grabPixels();
+
+    // Send the request to backend
+    axios.get('http://10.0.2.2:8080/ping').then(function (response) {
+      console.log(response.data.message);
+
+      // Show toast message on bottom of app
+      Snackbar.show({
+        text: 'Received response!',
+        duration: Snackbar.LENGTH_SHORT,
+      });
+    });
+  };
+
+  // Get image data from canvas
+  grabPixels = () => {
+    this.canvas.getBase64('jpg', false, true, false, false, (_err, result) => {
+      const resultImage = `data:image/jpg;base64,${result}`;
+      return resultImage;
+    });
+  };
+
   render() {
     return (
       <View style={styles.container}>
         <View style={styles.drawBox}>
           <SketchCanvas
+            ref={ref => (this.canvas = ref)}
             style={{flex: 1}}
             strokeColor={'black'}
             strokeWidth={15}
           />
         </View>
-        <Button title="Generate!" onPress={() => sendRequest()} />
+        <Button title="Generate!" onPress={() => this.sendRequest()} />
       </View>
     );
   }
