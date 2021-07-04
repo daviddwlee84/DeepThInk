aff94a35eceed7d7010665447305f633e1107832
Justin <justin.leung.ch@gmail.com>
2021-07-04 08:09:03 -0400

    Add basic server and api call to client

--- client/App.js
+++ client/App.js
@@ -1,23 +1,51 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
   AppRegistry,
   StyleSheet,
   View,
+  Button,
+  Alert,
+  Dimensions,
 } from 'react-native';
 
-import { SketchCanvas } from '@terrylinla/react-native-sketch-canvas';
+import {SketchCanvas} from '@terrylinla/react-native-sketch-canvas';
+import Snackbar from 'react-native-snackbar';
+import axios from 'axios';
+
+var device = Dimensions.get('window');
+
+// Sends a request to backend when clicking the "generate" button
+function sendRequest() {
+  console.log('Sending API request');
+  Snackbar.show({
+    text: 'Sending API request to server...',
+    duration: Snackbar.LENGTH_SHORT,
+  });
+
+  // Send the request to backend
+  axios.get('http://10.0.2.2:8080/ping').then(function (response) {
+    console.log(response.data.message);
+
+    // Show toast message on bottom of app
+    Snackbar.show({
+      text: 'Received response!',
+      duration: Snackbar.LENGTH_SHORT,
+    });
+  });
+}
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={{ flex: 1, flexDirection: 'row' }}>
+        <View style={styles.drawBox}>
           <SketchCanvas
-            style={{ flex: 1 }}
-            strokeColor={'red'}
-            strokeWidth={7}
+            style={{flex: 1}}
+            strokeColor={'black'}
+            strokeWidth={15}
           />
         </View>
+        <Button title="Generate!" onPress={() => sendRequest()} />
       </View>
     );
   }
@@ -25,7 +53,18 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#F5FCFF',
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'skyblue',
+  },
+  drawBox: {
+    aspectRatio: 1,
+    backgroundColor: 'white',
+    borderColor: 'lightblue',
+    borderWidth: 10,
+    width: device.width * 0.75,
+    height: device.width * 0.75,
   },
 });
-
---
+++ server/go.mod
@@ -0,0 +1,5 @@
+module main.com/m/v2
+
+go 1.16
+
+require github.com/gin-gonic/gin v1.7.2
