85098d3f9dfedd1bad601815f861ac9491c631b7
Justin <justin.leung.ch@gmail.com>
2021-07-14 01:58:59 -0400

    Reorganize model folder structure

---
+++ model/all_models/fast_neural_style/style_utils.py
@@ -0,0 +1,94 @@
+import sys
+import os
+import pathlib
+
+dir = pathlib.Path(__file__).parent.resolve()
+
+sys.path.append(
+    os.path.join(os.path.dirname(__file__), 'pytorch_examples', 'examples',
+                 'fast_neural_style', 'neural_style'))
+
+from vgg import Vgg16
+from transformer_net import TransformerNet
+import torch
+import re
+import cv2
+from neural_style import utils
+import torch
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision import transforms
+
+import cv2
+import matplotlib.pyplot as plt
+
+from PIL import Image
+import base64
+import io
+
+device = torch.device("cuda")
+# device = torch.device("cpu")
+
+# Loads pre-trained weights
+rain_princess_path = f'{dir}/saved_models/rain_princess.pth'
+candy_path = f'{dir}/saved_models/candy.pth'
+mosaic_path = f'{dir}/saved_models/mosaic.pth'
+udnie_path = f'{dir}/saved_models/udnie.pth'
+starry_night_path = f'{dir}/saved_models/starry_night.pth'
+
+
+# Loads the pre-trained weights into the fast neural style transfer
+# network architecture and puts the network on the Cloud TPU core.
+def load_style(path):
+    with torch.no_grad():
+        model = TransformerNet()
+        state_dict = torch.load(path)
+        # filters deprecated running_* keys from the checkpoint
+        for k in list(state_dict.keys()):
+            if re.search(r'in\d+\.running_(mean|var)$', k):
+                del state_dict[k]
+        model.load_state_dict(state_dict)
+        return model.to(device)
+
+
+# Creates each fast neural style transfer network
+rain_princess = load_style(rain_princess_path)
+candy = load_style(candy_path)
+mosaic = load_style(mosaic_path)
+udnie = load_style(udnie_path)
+# starry_night = load_style(starry_night_path)
+
+style_models = {
+    "rain_princess": rain_princess,
+    "candy": candy,
+    "mosaic": mosaic,
+    "udnie": udnie,
+}
+
+
+def stylizeImage(imageData: str, style: str):
+
+    base64_decoded = base64.b64decode(imageData)
+
+    # Load the content image
+    content_image = utils.load_image(io.BytesIO(base64_decoded), scale=None)
+    content_transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Lambda(lambda x: x.mul(255))])
+    content_image = content_transform(content_image)
+    content_image = content_image.unsqueeze(0).to(device)
+
+    # Generate the stylized image
+    with torch.no_grad():
+        #   output = rain_princess(content_image)
+        #   output = candy(content_image)
+        #   output = mosaic(content_image)
+
+        # Perform model inference
+        output = style_models[style](content_image)
+
+        # Get base64 string representation
+        output_str = utils.save_image_base64(output[0].cpu())
+
+    return output_str
--- model/app.py
+++ model/app.py
@@ -1,11 +1,15 @@
 """
-Flask deployment server for running deployed gauGAN model
+Flask server for running deployed gauGAN model
 """
 
-from model_utils import (processByte64, load_model, run_inference)
+from all_models.gaugan.model_utils import (processByte64, load_model,
+                                           run_inference)
+from all_models.fast_neural_style.style_utils import stylizeImage
 from flask import Flask, request
 from flask_cors import CORS
 import json
+import uuid
+import base64
 
 app = Flask(__name__)
 CORS(app)
@@ -21,17 +25,83 @@ def hello_world():
 
 @app.route('/generate', methods=['POST'])
 def generate():
+    """Process segmentation map into gauGAN to produce a generated image
+    
+    Args (in request.json):
+        imageData: base64 string representing a png image
+
+    Returns:
+        generatedData: base64 string representing a png generated image
+    """
+
+    # Generate a request id for saving the images
+    request_id = str(uuid.uuid4())
+
     # Fetch image data
     data = request.get_json()
-
     image_data = data.get("imageData")
 
+    # Save the image segmentation map
+    with open(f"outputs/{request_id}_SEGMENTATION.png", "wb") as fh:
+        fh.write(base64.urlsafe_b64decode(image_data))
+
+    # Convert byte64 into labelmap
     image_array = processByte64(image_data)
 
     # Perform inference
     generated_image = run_inference(image_array, model, opt)
 
+    # Remove the javascript file type header
+    generated_image_strip_header = generated_image.lstrip(
+        "data:image/png;base64")
+    # """
+    # STYLIZE
+    # """
+    # styled_image_str = stylizeImage(generated_image_strip_header,
+    #                                 "starry_night")['data']
+    # generated_image = styled_image_str
+    # generated_image_strip_header = styled_image_str.lstrip(
+    #     "data:image/png;base64")
+    # """
+    # END STYLIZE
+    # """
+
+    # Save the generated image
+    with open(f"outputs/{request_id}_GENERATED.png", "wb") as fh:
+        fh.write(base64.urlsafe_b64decode(generated_image_strip_header))
+
     return {"message": "Successfully got image", "data": generated_image}
+
+
+@app.route('/stylize', methods=['POST'])
+def stylize():
+    """Stylize an input image
+    
+    Args:
+        imageData: base64 str
+            The input base64 image string.
+        style: str ::=    "starry_night"
+                        | "rain_princess"
+                        | 
+            The requested style to stylize the input image with.
+    
+    Returns:
+
+    """
+    # Generate a request id for saving the images
+    request_id = str(uuid.uuid4())
+
+    # Fetch image data
+    data = request.get_json()
+    image_data = data.get("imageData")
+
+    style = data.get("style")
+
+    # Remove the javascript file type header
+    image_data_strip_header = image_data.lstrip("data:image/png;base64")
+
+    styled_image_str = stylizeImage(image_data_strip_header, style)
+    return {"message": "Successfully got image", "data": styled_image_str}
 
 
 if __name__ == "__main__":
