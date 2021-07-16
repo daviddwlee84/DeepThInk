6ef4477c6a83d7280a8569e2b1c7a83313d40540
Justin <justin.leung.ch@gmail.com>
2021-07-16 01:24:07 -0400

    Add example server

--- server/main.go
+++ server/main.go
@@ -1,61 +1,17 @@
 package main
 
 import (
-	"net/http"
 	"log"
-	"fmt"
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
-	"github.com/gorilla/websocket"
+	"net/http"
 )
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize: 1024,
-	WriteBufferSize: 1024,
-}
-
-func homePage(w http.ResponseWriter, r *http.Request) {
-	log.Println("hello")
-	fmt.Fprintf(w, "Home Page")
-}
-
-func reader(conn *websocket.Conn) {
-	for {
-		messageType, p, err := conn.ReadMessage()
-		if err != nil {
-			log.Println(err)
-			return
-		}
-
-		log.Println(string(p))
-
-		if err := conn.WriteMessage(messageType, p); err != nil {
-			log.Println(err)
-			return
-		}
-	}
-}
-
-func wsEndpoint(w http.ResponseWriter, r *http.Request) {
-	// allow any request, ignore cors
-	upgrader.CheckOrigin = func(r *http.Request) bool {
-        return true
-    }
-
-	ws, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Println(err)
-	}
-	
-	log.Println("Client successfully connected")
-	reader(ws)
-}
-
-
 func main() {
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-	r.Get("/", homePage)
-	r.Get("/ws", wsEndpoint)
-	http.ListenAndServe(":8080", r)
+	hub := newHub()
+	go hub.run()
+
+	http.HandleFunc("/ws", hub.handleWebSocket)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
