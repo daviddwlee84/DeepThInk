cbc3ba3ccd52ccbb506a896cf3d2c99200f0ada9
aprilslis <s826li@uwaterloo.ca>
2021-07-30 15:16:46 -0400

    icons

--- client-expo/App.js
+++ client-expo/App.js
@@ -215,7 +215,7 @@ export default class App extends Component {
             {/* Main canvas */}
             <DrawCanvas
               ref="drawCanvasRef"
-              style={{flex: 1}}
+              style="a useless field that cant style"
               thickness={this.state.thickness}
               color={this.state.color}
               socket={this.state.socket}
@@ -229,14 +229,14 @@ export default class App extends Component {
                   style={{
                     color: '#07235c',
                     fontWeight: 'bold',
-                    fontSize: device.height * 0.023,
+                    fontSize: device.height * 0.025,
                   }}>
                   Stroke size:
                 </Text>
                 <Slider
                   style={{
-                    width: device.width * 0.15,
-                    height: device.height * 0.03,
+                    width: device.width * 0.14,
+                    height: device.height * 0.028,
                     marginBottom: 10,
                   }}
                   minimumValue={1}
@@ -251,16 +251,74 @@ export default class App extends Component {
 
               <View style={styles.tempButtons}>
                 <View
-                  style={{justifyContent: 'flex-end', paddingHorizontal: 5}}>
-                  <Button color="#073ead" title="undo!" />
+                  style={{
+                    justifyContent: 'flex-end', 
+                    paddingHorizontal: 5,}}>
+                  <TouchableOpacity
+                    style={[styles.functionButton]}
+                    >
+                    <Text
+                      style={styles.tempIcons, {color: '#073ead'}}>
+                      <svg xmlns="http://www.w3.org/2000/svg" width={device.height * 0.03} height={device.height * 0.03} fill="currentColor" class="bi bi-arrow-counterclockwise" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M8 3a5 5 0 1 1-4.546 2.914.5.5 0 0 0-.908-.417A6 6 0 1 0 8 2v1z"/>
+                        <path d="M8 4.466V.534a.25.25 0 0 0-.41-.192L5.23 2.308a.25.25 0 0 0 0 .384l2.36 1.966A.25.25 0 0 0 8 4.466z"/>
+                      </svg>
+                    </Text>
+                  </TouchableOpacity> 
                 </View>
                 <View
                   style={{justifyContent: 'flex-end', paddingHorizontal: 5}}>
-                  <Button color="#073ead" title="redo!" />
+                  <TouchableOpacity
+                    style={[styles.functionButton]}
+                    >
+                    <Text
+                      style={styles.tempIcons, {color: '#073ead'}}>
+                      <svg xmlns="http://www.w3.org/2000/svg" width={device.height * 0.03} height={device.height * 0.03} fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
+                        <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
+                      </svg>
+                    </Text>
+                  </TouchableOpacity>
                 </View>
                 <View
                   style={{justifyContent: 'flex-end', paddingHorizontal: 5}}>
-                  <Button color="#07235c" title="erase" />
+                  <TouchableOpacity
+                    style={[styles.functionButton]}
+                    >
+                    <Text
+                      style={styles.tempIcons, {color: '#07235c',}}>
+                      <svg xmlns="http://www.w3.org/2000/svg" width={device.height * 0.03} height={device.height * 0.03} fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+                        <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
+                      </svg>
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+                <View
+                  style={{justifyContent: 'flex-end', paddingHorizontal: 5}}>
+                  <TouchableOpacity
+                    style={[styles.functionButton]}
+                    >
+                    <Text
+                      style={styles.tempIcons, {color: '#07235c',}}>
+                      <svg xmlns="http://www.w3.org/2000/svg" width={device.height * 0.03} height={device.height * 0.03} fill="currentColor" class="bi bi-eraser-fill" viewBox="0 0 16 16">
+                        <path d="M8.086 2.207a2 2 0 0 1 2.828 0l3.879 3.879a2 2 0 0 1 0 2.828l-5.5 5.5A2 2 0 0 1 7.879 15H5.12a2 2 0 0 1-1.414-.586l-2.5-2.5a2 2 0 0 1 0-2.828l6.879-6.879zm.66 11.34L3.453 8.254 1.914 9.793a1 1 0 0 0 0 1.414l2.5 2.5a1 1 0 0 0 .707.293H7.88a1 1 0 0 0 .707-.293l.16-.16z"/>
+                      </svg>
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+                <View
+                  style={{justifyContent: 'flex-end', paddingHorizontal: 5}}>
+                  <TouchableOpacity
+                    style={[styles.functionButton]}
+                    >
+                    <Text
+                      style={styles.tempIcons, {color: '#07235c',}}>
+                      <svg xmlns="http://www.w3.org/2000/svg" width={device.height * 0.03} height={device.height * 0.03} fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                        <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+                      </svg>
+                    </Text>
+                  </TouchableOpacity>
+
                 </View>
               </View>
             </View>
@@ -317,7 +375,7 @@ export default class App extends Component {
                       displayedImageData: this.state.generatedImageData,
                     }));
                   }}>
-                  <Text style={{color: 'white', fontSize: 20}}> None </Text>
+                  <Text style={{color: 'white', fontSize: device.height * 0.024,}}> None </Text>
                 </TouchableOpacity>
               </View>
               {/* Programmatically render all style options */}
@@ -359,11 +417,20 @@ export default class App extends Component {
           </View>
           {/* Generate button */}
           <View style={styles.genButton}>
-            <Button
-              color="#841584"
-              title="Generate!"
-              onPress={this.grabPixels.bind(this)}
-            />
+            <TouchableOpacity
+              style={[styles.functionButton, {backgroundColor: "#841584"}]}
+                onPress={this.grabPixels.bind(this)}
+              >
+              <Text
+                style={{
+                  color: 'white',
+                  alignItems: 'center',
+                  justifyContent: 'venter',
+                  fontSize: device.height * 0.035,
+                }}>
+                {"Generate!"}
+              </Text>
+            </TouchableOpacity>
           </View>
         </View>
       </View>
@@ -381,6 +448,7 @@ const styles = StyleSheet.create({
   },
 
   drawBox: {
+    flex: 1,
     backgroundColor: 'white',
     borderColor: 'lightblue',
     borderWidth: 10,
@@ -409,9 +477,10 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
   },
   genButton: {
-    height: 60,
-    alignItems: 'center',
+    borderRadius: 5,
+    paddingTop: 10,
     justifyContent: 'center',
+    alignItems: 'center',
   },
   strokeGroup: {
     flexDirection: 'column',
@@ -421,6 +490,10 @@ const styles = StyleSheet.create({
     height: 40,
     width: 70,
     flexDirection: 'row',
+  },
+  tempIcons: {
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   toolGroup: {
     flexDirection: 'row',
